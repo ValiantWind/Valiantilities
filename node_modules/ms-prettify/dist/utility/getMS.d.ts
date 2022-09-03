6597e3c40b7bbdf26a57333b052f6c54dcfee3ae
Pavan B <83314733+ValiantWind@users.noreply.github.com>
2022-09-03 00:08:56 +0000

    yes

---
+++ node_modules/ms-prettify/dist/utility/getMS.d.ts
@@ -0,0 +1,7 @@
+declare const _default: (time: number) => {
+    time: number;
+    value: number;
+    string: string;
+    type: string;
+};
+export default _default;
---
+++ node_modules/ms-prettify/dist/utility/toTimeString.d.ts
@@ -0,0 +1,2 @@
+import { ReturnObject } from '../interfaces';
+export default function (number: number, max: number, expanded: boolean, obj: boolean, till: "year" | "week" | "day" | "hour" | "minute" | "second" | "millisecond"): string | ReturnObject;
